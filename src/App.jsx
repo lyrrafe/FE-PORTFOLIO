0811aa0b33df1737b9e75dae312c3885a5d06336
LyrraFe <147235537+lyrrafe@users.noreply.github.com>
2025-03-06 22:59:26 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./Hero";
 import About from "./About";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
